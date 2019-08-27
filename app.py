962fc2f0215bd9d2b49f6d987deac6a3fac5ee35
Ssatyr <49284713+Ssatyr@users.noreply.github.com>
2019-08-27 09:34:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
+import flask
+app = flask.Flask(__name__)
 from . import views  # For import side-effects of setting up routes
